4d86e5dac6115f5ff0eedf8303a53e49cddb592c
Jember Projects <jemberalmojuela08@gmail.com>
2024-03-04 10:00:09 +0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Certs from "./components/Certs";
-import Contact from "./components/Contact";
-import Projects from "./components/Projects";
-import Fullstack from "./pages/fullstack";
-import Graphic from "./pages/graphicDesigning";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from "./components/Home";
+// import Certs from "./components/Certs";
+// import Contact from "./components/Contact";
+// import Projects from "./components/Projects";
+// import Fullstack from "./pages/fullstack";
+// import Graphic from "./pages/graphicDesigning";
+// import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="">
       <Navbar />
-      <Routes>
+
+      <a href="https://jember23.github.io/jemport/">Home</a>
+      <a href="jember23.github.io/jemport/Projects">Projects</a>
+      <a href="jember23.github.io/jemport/Certs">Certs</a>
+      <a href="jember23.github.io/jemport/Contact">Contact</a>
+
+
+      {/* <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Projects' element={<Projects/>} />
         <Route path='/Certs' element={<Certs/>} />
         <Route path='/Contact' element={<Contact/>} />
         <Route path='/fullstack' element={<Fullstack/>} />
         <Route path='/graphicDesigning' element={<Graphic/>} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
